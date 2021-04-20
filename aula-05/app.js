e9166c0c21dbc09de6cae5c6e86e3e93c4cb00a0
daniecdesouza <danielcdesouza@gmail.com>
2021-04-20 15:23:24 -0300

    Add lesson five

--- aula-05/app.js
+++ aula-05/app.js
@@ -4,5 +4,21 @@
 // console.log(JSON.stringify(estados)); // Transformar objeto em JSON
 // console.log(JSON.parse(estados)); //Transformar JSON em objeto
 
+// console.log("Digital House".length) // Recuperar o tamanho da String
+
+let cidades = "João Pessoa,São Paulo,Rio de Janeiro";
+// let cidadesDivididas = cidades.split(","); 
+
+// console.log(cidadesDivididas) //Cidades dividas pelo split
+let nome = "Daniel Cavalcante";
+// console.log(nome.slice(0, nome.length)) //Recuperar todos os characters da String
+
+let login = "   Dsouza@digitalhouse.com       ";
+// console.log(login)
+// console.log(login.trim()) // Remover espaços das laterais
+
+let email = "DSouza@digitalhouse.com"
+// console.log(email.toLowerCase()) // Transforma tudo em minusculo
+
 
 
