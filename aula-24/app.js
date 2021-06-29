056b7ab0db1ae15c637642efbcd6352beb65c4c2
Daniel Cavalcante <danielcdesouza@gmail.com>
2021-06-28 21:35:18 -0300

    Add code base to cookier parser

--- aula-24/app.js
+++ aula-24/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(session({ secret: "xuxinha_secret" }));
 
 app.get('/', (req, res) => {
@@ -34,7 +36,21 @@ app.post('/login', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.send(`Olá, ${req.session.user}!`)
+  res.send(`Olá, ${req.session.user.email}!`)
+})
+
+app.get('/settings', (req, res) => {
+
+  //Primeiro verificar se existe usuário na session
+  //Verificar se o usuário está no cookie
+  //Se não estiver em nenhum dos dois redirecionar para o /login
+  //Se conseguiu recuperar usuário, exibe as informações
+  res.render('settings', { theme: req.cookies.theme })
+})
+
+app.post('/settings', (req, res) => {
+  const { theme } = req.body;
+  res.cookie('theme', theme).send({ theme });
 })
 
 app.listen(port, () => {
