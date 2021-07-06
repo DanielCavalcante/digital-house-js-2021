ba3040605dc725bcc8328a40e0b987e5ce1e4a98
Daniel Cavalcante <danielcdesouza@gmail.com>
2021-07-05 21:37:47 -0300

    Add other files

---
+++ aula-26/asyncAwait.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+const url = 'https://pokeapi.co/api/v2/pokemon';
+
+const getPokes = async () => {
+  try {
+    const res = await fetch(url);
+    const pokes = await res.json();
+    console.log(pokes.results);
+
+    //
+
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getPokes();
